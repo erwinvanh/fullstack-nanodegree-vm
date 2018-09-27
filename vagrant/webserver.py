1ff7a9d8504b78450655f9489100e19110b305f1
Erwin van Herwijnen <erwin.van.herwijnen@gmail.com>
2018-09-27 15:12:44 +0200

    First commit

---
+++ vagrant/webserver.py
@@ -0,0 +1,33 @@
+#from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class WebServerHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        print('Received request')
+        if self.path.endswith("/hello"):
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            message = ""
+            message += "<html><body>Hello!</body></html>"
+            self.wfile.write(bytes(message, " utf-8"))
+            print(message)
+            return
+        else:
+            self.send_error(404, 'File Not Found: %s' % self.path)
+
+
+def main():
+    try:
+        print("Starting webserver")
+        port = 9000
+        server = HTTPServer(('', port), WebServerHandler)
+        print("Web Server running on port %s" % port)
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print(" ^C entered, stopping web server....")
+        server.socket.close()
+
+if __name__ == '__main__':
+    main()
